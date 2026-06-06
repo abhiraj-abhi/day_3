7b5d15ecf5d0acdc224849584f09f0a750956d7f
Your Name <your_email@example.com>
2026-06-06 00:23:59 -0700

    Created basic python program

---
+++ python_basics.py
@@ -0,0 +1,8 @@
+print("Name: Abhiraj")
+print("College: Your College Name")
+age = 20
+branch = "Bvoc Mad"
+print("Name:", "Abhiraj")
+print("College:", "Amal College")
+print("Age:", age)
+print("Branch:", branch)
