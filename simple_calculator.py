71b62f578d18a30f48f5e0c953a246cf76b9a6f0
Your Name <your_email@example.com>
2026-06-06 02:24:20 -0700

    Added simple calculator program

---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
+print("\n===== Calculator Results =====")
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division       :", num1 / num2)
+print("==============================")
