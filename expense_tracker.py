43c93df067e0ae354966e9e80056931225872635
Your Name <your_email@example.com>
2026-06-06 03:39:03 -0700

    Added expense tracker program

---
+++ expense_tracker.py
@@ -0,0 +1,13 @@
+food_expense = float(input("Enter Food Expense: "))
+travel_expense = float(input("Enter Travel Expense: "))
+study_expense = float(input("Enter Study Expense: "))
+
+total_expense = food_expense + travel_expense + study_expense
+
+print("\n===== Expense Summary =====")
+print("Food Expense   :", food_expense)
+print("Travel Expense :", travel_expense)
+print("Study Expense  :", study_expense)
+print("---------------------------")
+print("Total Expense  :", total_expense)
+print("===========================")
